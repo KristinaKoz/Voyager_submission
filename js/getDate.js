a98701f1d0ca466980cb7204ca13ccd9808bb151
Hazel <Hazel.okeeffe@mycit.ie>
2019-12-16 16:58:43 +0100

    V4 commit from Slack
    This version is based on the last version from Slack. Many changes have been made to ensure that the site now validates. There are still Javascript issues to be resolved.

--- js/getDate.js
+++ js/getDate.js
@@ -1,25 +1,27 @@
+//JavaScript code for Date and Time
+//Contributed by Hazel O'Keeffe. 
+
 function date_time(id) {
+    // This returns a Javascript Date object, and assigns it to the "date" variable
     date = new Date;
-    year = date.getFullYear();
+    
+    // These methods return the month, day and date from the Date object, and assign respectively to the month, day and d variables.
     month = date.getMonth();
-    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-    d = date.getDate();
     day = date.getDay();
+    d = date.getDate();
+    
+    // Here we're creating two arrays - one for the names of the months and one for the names of the days so we can display them.
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
     days = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
-    h = date.getHours();
-    if (h < 10) {
-        h = "0" + h;
-    }
-    m = date.getMinutes();
-    if (m < 10) {
-        m = "0" + m;
-    }
-    s = date.getSeconds();
-    if (s < 10) {
-        s = "0" + s;
-    }
-    result = '' + days[day] + ', ' + months[month] + ' ' + d;
+
+    // We create a results variable and adding the relevant information to it in the correct format
+    // We use the numbers returned from the getDay and getMonth functions to find the right month and day names from the array.
+    result = '' + days[day] + ', ' + months[month] + ' '+d;
+    
+    //Here we're adding the result we've just created to the correct id on the webpage.
     document.getElementById(id).innerHTML = result;
+    
+    //Here we set a timeout to ensure that the date refreshes.
     setTimeout('date_time("' + id + '");', '1000');
     return true;
 }
