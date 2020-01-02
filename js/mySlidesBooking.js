085d171700336dcb60bc009f8be8af373b6e5617
Hazel <Hazel.okeeffe@mycit.ie>
2020-01-02 12:36:25 +0100

    Merging in Kristina Kozaj's Booking changes

--- js/mySlidesBooking.js
+++ js/mySlidesBooking.js
@@ -1,19 +1,29 @@
 //JavaScript code for Automatic Slideshow ("Book your Ticket" Page)
-//This code was heavily inspired from this w3schools article: https://www.w3schools.com/howto/howto_js_slideshow.asp
+//This code was heavily inspired from this w3schools article:https://www.w3schools.com/w3css/w3css_slideshow.asp
 
-var slideIndex = 0;
-carousel();
 
-function carousel()
-{
-  var i;
-  var x = document.getElementsByClassName("mySlidesBooking");
-  for (i = 0; i < x.length; i++)
-  {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 4000); 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlidesBooking");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("", "");
+    }
+    x[slideIndex - 1].style.display = "block";
 }
